78ea0310897516a0a46bd401ba8df7c5ededabfa
Simon Kaine <simonkaine@gmail.com>
2021-09-24 17:46:41 -0700

    should DELETE an order by id + pass test.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -67,4 +67,13 @@ describe('03_separation-of-concerns-demo routes', () => {
       });
   });
 
+  it('should DELETE an order by id', async () => {
+    await request(app).post('/api/v1/orders').send({ id: 1, quantity: 66 });
+    return request(app)
+      .delete('/api/v1/orders/1')
+      .then(res => {
+        expect(res.body).toEqual({});
+      });
+  });
+
 });
